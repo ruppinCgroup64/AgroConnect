8250d467ffd749a0069e7909f589f0fe03594374
Adi Hadad <adihadad5566@gmail.com>
2024-08-21 21:49:31 +0300

    code with git ignore

---
+++ AgroConnect-app-server/AgroConnect-server/BL/FairOrganizers.cs
@@ -0,0 +1,14 @@
+﻿using AgroConnect_server.BL;
+
+public class FairOrganizers : Consumers
+{
+    
+
+    public FairOrganizers(string email, string firstName, string lastName, string password, string gender, string dateOfBirth, string phoneNum, string address, string registrationDate, bool isAdmin, string profilePic, bool isFarmer, string longitude, string latitude)
+        : base(email, firstName, lastName, password, gender, dateOfBirth, phoneNum, address, registrationDate, isAdmin, profilePic, isFarmer, longitude, latitude)
+    {
+        
+    }
+
+    public FairOrganizers() { }
+}
---
+++ AgroConnect-app-server/AgroConnect-server/Controllers/UploadController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdHocObjects.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        // GET: api/<UploadController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<UploadController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UploadController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] List<IFormFile> files)
+        {
+
+
+            List<string> imageLinks = new List<string>();
+
+            string path = System.IO.Directory.GetCurrentDirectory();
+
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.Combine(path, "uploadedFiles/"+formFile.FileName);
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                    imageLinks.Add(formFile.FileName);
+                }
+            }
+
+            // Return status code  
+            return Ok(imageLinks);
+
+        }
+
+        // PUT api/<UploadController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UploadController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
